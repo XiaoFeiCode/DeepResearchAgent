1a39bf3210a4487e50a734d120e2fa0408869dbf
yangbo <3186674656@qq.com>
2026-06-03 13:56:03 +0800

    first using deepagent

---
+++ base/deepagent.py
@@ -0,0 +1,87 @@
+from typing import Literal
+
+from langchain.chat_models import init_chat_model
+from deepagents import create_deep_agent
+from dotenv import find_dotenv, load_dotenv
+from langchain_core.tools import tool
+from tavily import TavilyClient
+
+import os
+
+# 使用 find_dotenv() 自动查找 .env 文件，无论你在哪个目录下运行脚本都能正确加载环境变量
+load_dotenv(find_dotenv())
+
+tavily_client = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
+
+
+@tool
+def internet_search(
+    query: str,
+    max_results: int = 10,
+    topic: Literal["general", "news", "finance"] = "general",
+    include_raw_content: bool = False,
+):
+    """
+    互联网搜索工具！
+    :param query: 搜索关键字
+    :param max_results: 返回结果数量
+    :param topic: 主题类型
+    :param include_raw_content: False精简 True 返回详细结果
+    :return: 搜索结果列表
+    """
+    print(
+        f"进行网络搜索！搜索条件：{query},"
+        f"搜索主题类别:{topic},"
+        f"搜索最大的条数：{max_results}"
+    )
+    return tavily_client.search(
+        query=query,
+        max_results=max_results,
+        topic=topic,
+        include_raw_content=include_raw_content,
+    )
+
+
+# 极简初始化（自动读取OPENAI环境变量）
+llm = init_chat_model(
+    model=os.getenv("LLM_QWEN_MAX"),
+    model_provider="openai"
+)
+
+# 功能等价于langchain的 create_agent
+deep_agent = create_deep_agent(
+    model=llm,
+    tools=[internet_search],
+    subagents=[],
+    system_prompt="""
+      你是一位专家级研究员。你的任务是进行深入研究并撰写一份精美的报告。
+      你有权使用 internet_search 工具来收集信息。
+    """
+)
+
+prompt = input("输入你关系的问题：")
+result = deep_agent.invoke(
+    {
+        "messages":[
+        {"role":"user","content":f"{prompt}"}
+    ]
+    }
+)
+
+"""
+结果数据说明
+ {
+    "messages": [
+        # 第0条：你的提问（HumanMessage）
+        HumanMessage(content='搜索宇树机器人的新闻！'),
+        # 第1条：Agent 调用工具的指令（AIMessage，内容为空，仅触发工具）
+        AIMessage(content='', tool_calls=[{'name':'internet_search', ...}]),
+        # 第2条：工具返回的搜索结果（ToolMessage，一堆JSON数据）
+        ToolMessage(content='{"query":"宇树机器人 新闻","results":[...]}'),
+        # 第3条：Agent 整理后的最终回复（AIMessage，这是你要的内容）
+        AIMessage(content='以下是关于宇树机器人的一些最新新闻：...')
+    ]
+}
+"""
+
+print(print(result['messages'][-1].content))
